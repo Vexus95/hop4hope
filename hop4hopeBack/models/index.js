f3ff8d488aa05e8eabcc9d33fb4b882f9ad9e20c
DamienBataillard <damien.bataillard@efrei.net>
2025-01-30 10:44:00 +0100

    Update index.js

--- hop4hopeBack/models/index.js
+++ hop4hopeBack/models/index.js
@@ -2,8 +2,19 @@ const { sequelize } = require('../config/database');
 const Utilisateur = require('./Utilisateur');
 const Personnage = require('./Personnage');
 const Animation = require('./Animation');
+const Quete = require('./Quete');
+const Avoir = require('./Avoir');
+const Possede = require('./Possede');
 
-// Relations entre les modèles
+// Relations Utilisateur <--> Quête (Many-to-Many)
+Utilisateur.belongsToMany(Quete, { through: Avoir, foreignKey: 'Id_utilisateur' });
+Quete.belongsToMany(Utilisateur, { through: Avoir, foreignKey: 'Id_quete' });
+
+// Relations Utilisateur <--> Personnage (Many-to-Many)
+Utilisateur.belongsToMany(Personnage, { through: Possede, foreignKey: 'Id_utilisateur' });
+Personnage.belongsToMany(Utilisateur, { through: Possede, foreignKey: 'Id_personnage' });
+
+// Relation Personnage <--> Animation (One-to-Many)
 Personnage.hasMany(Animation, { foreignKey: 'Id_personnage' });
 Animation.belongsTo(Personnage, { foreignKey: 'Id_personnage' });
 
@@ -12,4 +23,7 @@ module.exports = {
     Utilisateur,
     Personnage,
     Animation,
+    Quete,
+    Avoir,
+    Possede,
 };
