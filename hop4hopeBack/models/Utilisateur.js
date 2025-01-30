ab1927bad33f75cca46fcb048cbb216f4d7b815b
DamienBataillard <damien.bataillard@efrei.net>
2025-01-30 11:02:31 +0100

    Update Utilisateur.js

--- hop4hopeBack/models/Utilisateur.js
+++ hop4hopeBack/models/Utilisateur.js
@@ -7,6 +7,7 @@ const Utilisateur = sequelize.define('Utilisateur', {
     Prénom: { type: DataTypes.STRING(200), allowNull: false },
     Email: { type: DataTypes.STRING(200), allowNull: false, unique: true },
     motdepasse: { type: DataTypes.STRING(200), allowNull: false },
+    points: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 } // New field for quest points
 }, { timestamps: false });
 
 module.exports = Utilisateur;
