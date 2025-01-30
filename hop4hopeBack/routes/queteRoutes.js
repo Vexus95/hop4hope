5afbe7f4217613973180ec711957d33c371f06b3
DamienBataillard <damien.bataillard@efrei.net>
2025-01-30 11:22:38 +0100

    Update queteRoutes.js

--- hop4hopeBack/routes/queteRoutes.js
+++ hop4hopeBack/routes/queteRoutes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 
 // Route to update quest progress
 router.post('/progress', authenticateUser, updateQuestProgress);
+// Route to claim quest reward
+router.post('/claim-reward', authenticateUser, claimQuestReward);
 
 module.exports = router;
