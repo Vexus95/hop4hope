59a3435f215c25ab7ef34aadf6315f81440e9164
DamienBataillard <damien.bataillard@efrei.net>
2025-01-30 10:53:29 +0100

    Update server.js

--- hop4hopeBack/server.js
+++ hop4hopeBack/server.js
@@ -1,18 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const { connectDB } = require('./config/database');
 const personnageRoutes = require('./routes/personnageRoutes');
 const animationRoutes = require('./routes/animationRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Routes
+app.use('/users', userRoutes);
 app.use('/personnages', personnageRoutes);
 app.use('/animations', animationRoutes);
 
-// Lancer le serveur
-const PORT = 3000;
+// Start server
+const PORT = 5000;
 app.listen(PORT, async () => {
     await connectDB();
-    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+    console.log(`Backend running at http://localhost:${PORT}`);
 });
