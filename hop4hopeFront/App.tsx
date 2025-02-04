e8e86b297663b7c0170ad792bed7ab0a4066ccda
Olivier De Ménibus <91889834+oliviercmoi@users.noreply.github.com>
2025-02-04 03:26:54 +0100

    bt

--- hop4hopeFront/App.tsx
+++ hop4hopeFront/App.tsx
@@ -7,6 +7,10 @@ import DanceScreen from './src/screens/DanceScreen';
 import MyDeviceScreen from './src/screens/MyBraceletScreen';
 import QuestsScreen from './src/screens/QuestsScreen';
 import BluetoothScreen from './src/screens/BluetoothScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import ShopScreen from './src/screens/ShopScreen';
+import CreateAccountScreen from './src/screens/CreateAccountScreen';
+import BluetoothScreen from './src/screens/BluetoothScreen';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -22,6 +26,22 @@ function MainTabs() {
     </Tab.Navigator>
   );
 }
+export type RootStackParamList = {
+  Login: undefined;
+  CreateAccount: undefined;
+  // Ajoute d'autres routes ici si besoin
+};
+
+
+const MainApp = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="MyDevice" component={BluetoothScreen} />
+    <Tab.Screen name="Quests" component={QuestsScreen} />
+    <Tab.Screen name="Wardrobe" component={WardrobeScreen} />
+    <Tab.Screen name="Dance" component={DanceScreen} />
+    <Tab.Screen name="Shop" component={ShopScreen} />
+  </Tab.Navigator>
+);
 
 export default function App() {
   return (
