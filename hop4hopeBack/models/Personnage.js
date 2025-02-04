3696b8b62b9cae25f5584cfa88ee59ba50333722
DamienBataillard <damien.bataillard@efrei.net>
2025-02-04 11:02:01 +0100

    Update Personnage.js

--- hop4hopeBack/models/Personnage.js
+++ hop4hopeBack/models/Personnage.js
@@ -6,8 +6,8 @@ const Personnage = sequelize.define('Personnage', {
     Nom: { type: DataTypes.STRING(200), allowNull: false },
     coût: { type: DataTypes.INTEGER, allowNull: false },
     matrice: { type: DataTypes.TEXT, allowNull: false },
+    image: { type: DataTypes.STRING(500), allowNull: true },
     activeAnimationId: { type: DataTypes.INTEGER, allowNull: true }
 }, { timestamps: false });
-
 
 module.exports = Personnage;
